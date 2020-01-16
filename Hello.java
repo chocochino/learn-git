581d72f12290decb79c3dcb439d702457e17a3fc
Livia Lohanda <livia.lohanda@uph.edu>
2020-01-16 08:00:07 +0700

    Add content in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!\n");
+		System.out.println("Halo, dunia!\n");
+		System.out.println("This is a change to be commited.");
 	}
 }
